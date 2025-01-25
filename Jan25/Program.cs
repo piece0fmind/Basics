93fc1b51327c8035e416a03949d354eef8ae1a85
piece0fmind <bibek9lives@icloud.com>
2025-01-25 14:38:53 +0545

    Define 4 types of functions for the same output

---
+++ Jan25/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Jan25;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Input 2 numbers to add:");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Calling AddInputNumbers");
+Functions.AddInputNumbers(a, b);
+
+Console.WriteLine("Calling AddNumbers");
+Functions.AddNumbers();
+Console.WriteLine("Calling AddInputAndReturn");
+int sum = Functions.AddInputAndReturn(a, b);
+Console.WriteLine($"The sum of {a} and {b} is {sum}");
+
+
+void AddInputNumbers(int a, int b) // takes parameters but doesn't return
+{
+    int sum = a + b;
+    Console.WriteLine($"The sum of {a} and {b} is {sum}");
+}
+void AddNumbers() // doesn't take parameters and doesn't return
+{
+    int a = 10;
+    int b = 20;
+    int sum = a + b;
+    Console.WriteLine($"The sum of {a} and {b} is {sum}");
+}
+
+int AddAndReturn() // doesn't take parameters but returns
+{
+    int a = 10;
+    int b = 20;
+    int sum = a + b;
+    return sum;
+}
+int AddInputAndReturn(int a, int b) // takes parameters and returns 
+{
+    int sum = a + b;
+    return sum;
+}
+
