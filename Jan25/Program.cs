7d606995a1af2d95a33b245dd6aea347fe5ed905
Sushil Bhujel <vjlsushil@gmail.com>
2025-01-27 21:09:59 +0545

    Resolve Merge Conflict

--- Jan25/Program.cs
+++ Jan25/Program.cs
@@ -2,8 +2,8 @@
 using BasicExamples.Jan26;
 using Jan25;
 
-//ExecuteJan25();
-ExecuteJan26();
+ExecuteJan25();
+
 
 void ExecuteJan25()
 {
@@ -11,11 +11,17 @@ void ExecuteJan25()
 
     int a = Convert.ToInt32(Console.ReadLine());
     int b = Convert.ToInt32(Console.ReadLine());
+
     Console.WriteLine("Calling AddInputNumbers");
     Functions.AddInputNumbers(a, b);
 
+
     Console.WriteLine("Calling AddNumbers");
-    Functions.AddNumbers();
+    int sums = Functions.AddAndReturn();
+    Console.WriteLine($"The sum is: {sums}");
+
+
+
     Console.WriteLine("Calling AddInputAndReturn");
     int sum = Functions.AddInputAndReturn(a, b);
     Console.WriteLine($"The sum of {a} and {b} is {sum}");
@@ -23,24 +29,25 @@ void ExecuteJan25()
 
 }
 
-void ExecuteJan26()
-{
-    Console.WriteLine("Print an array");
-    int[] array = new int[5]; // length of array is necessary while creating
-    Console.WriteLine("Enter 5 numbers:");
-    for (int i = 0; i < 5; i++)
-    {
-        int number = Convert.ToInt32(Console.ReadLine());
-        array[i] = number;
-    }
-    Arrays.Print(array);
+//ExecuteJan26();
+//void ExecuteJan26()
+//{
+//    Console.WriteLine("Print an array");
+//    int[] array = new int[5]; // length of array is necessary while creating
+//    Console.WriteLine("Enter 5 numbers:");
+//    for (int i = 0; i < 5; i++)
+//    {
+//        int number = Convert.ToInt32(Console.ReadLine());
+//        array[i] = number;
+//    }
+//    Arrays.Print(array);
 
-    Console.WriteLine("Counting length of the array");
-    int length = Arrays.CalculateLength(array);
-    Console.WriteLine($"The length of the array is {length}");
+//    Console.WriteLine("Counting length of the array");
+//    int length = Arrays.CalculateLength(array);
+//    Console.WriteLine($"The length of the array is {length}");
 
-    Console.WriteLine("Finding the max number in the array");
-    int max = Arrays.FindMax(array);
-    Console.WriteLine($"The max number in the array is {max}");
-}
+//    Console.WriteLine("Finding the max number in the array");
+//    int max = Arrays.FindMax(array);
+//    Console.WriteLine($"The max number in the array is {max}");
+//}
 
